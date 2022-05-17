4c2e874b66077f289cb0381ca6aec6c3d66a2a92
patricia-mps <patricia@esolidar.pt>
2022-05-17 16:47:34 +0100

    add new component DonationModal

---
+++ src/pages/patricia/index.tsx
@@ -0,0 +1,11 @@
+import DonationModal from '../../components/donationModal/DonationModal';
+
+const Home = () => {
+  return (
+    <div>
+      <DonationModal openModal={true} />
+    </div>
+  );
+};
+
+export default Home;
