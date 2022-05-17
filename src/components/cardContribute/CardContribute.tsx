c2da51423948fc6773475b526b32d552dfe91d9f
Pedro Guia <54513333+pedroguia@users.noreply.github.com>
2022-05-17 12:58:17 +0100

    Add: improved nonprofit detail

--- src/components/cardContribute/CardContribute.tsx
+++ src/components/cardContribute/CardContribute.tsx
@@ -1,17 +1,32 @@
 import Icon from '@esolidar/toolkit/build/elements/icon';
 import Button from '@esolidar/toolkit/build/elements/button';
 import truncateAddress from '../../utils/truncateAddress';
+import useToast from '../../hooks/useToast/useToast';
 
 interface Props {
   name: string;
   address: string;
   onClickDonate(): void;
+  onClickShare(): void;
 }
 
-const CardContribute = ({ name, address, onClickDonate }: Props) => {
+const CardContribute = ({ name, address, onClickDonate, onClickShare }: Props) => {
+  const toast = useToast();
+
   // const valora = `https://chart.googleapis.com/chart?chs=160x160&cht=qr&chl=celo://wallet/pay?address=0x7F38B1585d55A9bc881da27e2FB927d0db30fD41&displayName=esolidar&chld=L%7C0`;
   const metamask = `https://chart.googleapis.com/chart?chs=160x160&cht=qr&chl=wc:099df4c1-8d6d-4432-b6b1-f8fe5e243efe@1?bridge=https%3A%2F%2Fz.bridge.walletconnect.org&key=2625629f0670a8a5d660471621a727effbdaf28d32b23fa7a34ccfa143779bb7
   &displayName=esolidar&chld=L%7C0`;
+
+  const handleClickAddress = () => {
+    // const url = `https://explorer.celo.org/address/${address}/transactions`;
+    const url = String(process.env.NEXT_PUBLIC_ADDRESS_DETAILS_URL).replace('{{address}}', address);
+    window.open(url, '_blank');
+  };
+
+  const handleCopyAddress = () => {
+    navigator.clipboard.writeText(address);
+    toast.success('Successfully copied address');
+  };
 
   return (
     <div className="card-contribute">
@@ -21,11 +36,11 @@ const CardContribute = ({ name, address, onClickDonate }: Props) => {
       </p>
       <img className="card-contribute__qr-code" alt={String(address)} src={metamask} />
       <div className="card-contribute__address">
-        <Button extraClass="link" text={truncateAddress(address, 5)} onClick={() => {}} />
+        <Button extraClass="link" text={truncateAddress(address, 5)} onClick={handleClickAddress} />
         <Button
           extraClass="primary-full"
           icon={<Icon name="Copy" size="sm" />}
-          onClick={() => {}}
+          onClick={handleCopyAddress}
           ghost
           size="sm"
           type="icon"
@@ -42,7 +57,7 @@ const CardContribute = ({ name, address, onClickDonate }: Props) => {
         <Button
           extraClass="secondary"
           text="Share"
-          onClick={() => {}}
+          onClick={onClickShare}
           size="lg"
           iconLeft={<Icon name="Share3" />}
           fullWidth
