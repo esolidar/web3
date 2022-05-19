ab124af74c0fb929b7ca28824831ead2e6814c75
Pedro Guia <54513333+pedroguia@users.noreply.github.com>
2022-05-19 11:01:09 +0100

    Add: improved changing locales

--- src/components/cardContribute/CardContribute.tsx
+++ src/components/cardContribute/CardContribute.tsx
@@ -1,4 +1,6 @@
+// import { useIntl } from 'react-intl';
 import Icon from '@esolidar/toolkit/build/elements/icon';
+import Popover from '@esolidar/toolkit/build/elements/popover';
 import Button from '@esolidar/toolkit/build/elements/button';
 import truncateAddress from '../../utils/truncateAddress';
 import useToast from '../../hooks/useToast/useToast';
@@ -11,14 +13,14 @@ interface Props {
 }
 
 const CardContribute = ({ name, address, onClickDonate, onClickShare }: Props) => {
+  // const intl = useIntl();
   const toast = useToast();
 
   // const valora = `https://chart.googleapis.com/chart?chs=160x160&cht=qr&chl=celo://wallet/pay?address=0x7F38B1585d55A9bc881da27e2FB927d0db30fD41&displayName=esolidar&chld=L%7C0`;
-  const metamask = `https://chart.googleapis.com/chart?chs=160x160&cht=qr&chl=wc:099df4c1-8d6d-4432-b6b1-f8fe5e243efe@1?bridge=https%3A%2F%2Fz.bridge.walletconnect.org&key=2625629f0670a8a5d660471621a727effbdaf28d32b23fa7a34ccfa143779bb7
-  &displayName=esolidar&chld=L%7C0`;
+  // const metamask = `https://chart.googleapis.com/chart?chs=160x160&cht=qr&chl=wc:099df4c1-8d6d-4432-b6b1-f8fe5e243efe@1?bridge=https%3A%2F%2Fz.bridge.walletconnect.org&key=2625629f0670a8a5d660471621a727effbdaf28d32b23fa7a34ccfa143779bb7&displayName=esolidar&chld=L%7C0`;
+  const qrCodeImage = `https://chart.googleapis.com/chart?chs=160x160&cht=qr&chl=${address}&displayName=esolidar&chld=L%7C0`;
 
   const handleClickAddress = () => {
-    // const url = `https://explorer.celo.org/address/${address}/transactions`;
     const url = String(process.env.NEXT_PUBLIC_ADDRESS_DETAILS_URL).replace('{{address}}', address);
     window.open(url, '_blank');
   };
@@ -30,11 +32,23 @@ const CardContribute = ({ name, address, onClickDonate, onClickShare }: Props) =
 
   return (
     <div className="card-contribute">
-      <h3 className="card-contribute__header">Contribute in Celo Dollar </h3>
+      <h3 className="card-contribute__header">
+        Contribute in Celo Dollar
+        <Popover
+          overlayTrigger={<Icon name="InfoBold" size="sm" />}
+          size="md"
+          popoverBodyChildren={
+            <p>
+              Send only cUSD to this address.Sending other unsupported tokens to this address may
+              result in the loss of your donation.
+            </p>
+          }
+        />
+      </h3>
       <p className="card-contribute__header-description body-small">
         100% of your donation goes to {name}
       </p>
-      <img className="card-contribute__qr-code" alt={String(address)} src={metamask} />
+      <img className="card-contribute__qr-code" alt={String(address)} src={qrCodeImage} />
       <div className="card-contribute__address">
         <Button extraClass="link" text={truncateAddress(address, 5)} onClick={handleClickAddress} />
         <Button
--- src/utils/locales.ts
+++ src/utils/locales.ts
@@ -3,17 +3,17 @@ import { SUPPORTED_LOCALES } from '../constants/locales';
 
 const getLocaleTranslations = (lang: string) => {
   switch (lang) {
-    case SUPPORTED_LOCALES.BR:
+    case SUPPORTED_LOCALES.BR.id:
       return Object.assign(
         require('@esolidar/i18n/projects/toolkit/br'),
         require('@esolidar/i18n/projects/web3/br')
       );
-    case SUPPORTED_LOCALES.EN:
+    case SUPPORTED_LOCALES.EN.id:
       return Object.assign(
         require('@esolidar/i18n/projects/toolkit/en'),
         require('@esolidar/i18n/projects/web3/en')
       );
-    case SUPPORTED_LOCALES.PT:
+    case SUPPORTED_LOCALES.PT.id:
       return Object.assign(
         require('@esolidar/i18n/projects/toolkit/pt'),
         require('@esolidar/i18n/projects/web3/pt')
