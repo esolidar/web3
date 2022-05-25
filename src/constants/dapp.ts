4c399ef789a8736c5708257e987b7d50aa79208f
apoca <vieira@esolidar.com>
2022-05-25 11:47:26 +0100

    added image to readme

--- src/constants/dapp.ts
+++ src/constants/dapp.ts
@@ -1,2 +1,2 @@
-export const DAPP_NAME: string = 'esolidar web3';
+export const DAPP_NAME: string = 'esolidar';
 export const DAPP_DESCRIPTION: string = 'Empowering philanthropy and ESG practice through blockchain';
