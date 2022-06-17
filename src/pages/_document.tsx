29d8bb5fcec3fd9c22dd13766ea223769f76a0be
Pedro Guia <54513333+pedroguia@users.noreply.github.com>
2022-06-17 10:47:55 +0100

    ENG-2047 #comment Add: added font smoothing and commented intercom scripts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -32,7 +32,7 @@ const Document = () => (
             `,
         }}
       />
-      <script
+      {/* <script
         dangerouslySetInnerHTML={{
           __html: `
         window.intercomSettings = {
@@ -41,7 +41,7 @@ const Document = () => (
         };
             `,
         }}
-      />
+      /> */}
 
       <script
         dangerouslySetInnerHTML={{
@@ -76,13 +76,13 @@ const Document = () => (
     <body>
       <Main />
       <NextScript />
-      <script
+      {/* <script
         dangerouslySetInnerHTML={{
           __html: `
         (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/dzl5fev0';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
         `,
         }}
-      />
+      /> */}
     </body>
   </Html>
 );
